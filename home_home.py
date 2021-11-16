b4129ea87bf5ef7dfec98f7df3ec7a71a08ac55f
as3594 <as3594@g.rit.edu>
2021-11-16 10:57:34 +0400

    added dictionary for garden, bathroom,indoor

--- home_home.py
+++ home_home.py
@@ -1,5 +1,7 @@
 
-d={}
+dg={'p':['3 pack garden flower','$5.0'],'l':['hanging light wire','$10.0'],'b':['garden bench','$35.0'],'n':['none and next','$0']}
+di={'t':['Small table lamp','$5.0'],'f1':['City picture frame','$7.0'],'r':['4x5 entry rug','$35.0'],'f2':['flower vase','$14.0'],'n':['none and next','$0']}
+db = {'c':['shower curtains','$8.0'],'m':['wall mounted mirror','$20.0'],'s':['marble sink','$40.0'],'n':['none and next','$0']}
 
 class home:
     __slots__ = ['garden','indoor','bathroom']
@@ -10,3 +12,5 @@ class item_state:
     name=''
     code=''
     price=0
+
+class home_avatar:
