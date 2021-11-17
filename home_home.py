ac921214edb3f9e8eb4bd25cbaa79b0c4ec5980e
as3594 <as3594@g.rit.edu>
2021-11-17 17:14:33 +0400

    added menu for garden

--- home_home.py
+++ home_home.py
@@ -4,20 +4,27 @@ di={'t':['Small table lamp','$5.0'],'f1':['City picture frame','$7.0'],'r':['4x5
 db = {'c':['shower curtains','$8.0'],'m':['wall mounted mirror','$20.0'],'s':['marble sink','$40.0'],'n':['none and next','$0']}
 
 class home:
-    __slots__ = ['garden','indoor','bathroom']
+    __slots__ = []
 
 class home_category:
-    __slots__ = []
+    __slots__ = ['garden','indoor','bathroom']
 
 class item_state():
     __slots__ = ['name','code','price']
-    def __init__(self,name,code,price):
-        self.name=name
+    def __init__(self,code):
         self.code=code
-        self.price=price
+        self.name= name
+        self.price= price
 
 
 class home_avatar:
+    pass
+
+def menu():
+    for i in dg:
+       print(i,', item=',dg[i][0],', price=',dg[i][1])
+
+menu()
 
 def main():
-    item1=item_state.
+    item1=item_state('p')
