223cbe4ac2f157a20cb0bf1b6e4b18f5f6d13b19
advikasunil <as3594@g.rit.edu>
2021-11-22 10:55:23 +0400

    while loop for garden and indoor works

--- home_home.py
+++ home_home.py
@@ -1,7 +1,7 @@
 
-dg={'p':['3 pack garden flower','$5.0'],'l':['hanging light wire','$10.0'],'b':['garden bench','$35.0'],'n':['none and next','$0']}
-di={'t':['Small table lamp','$5.0'],'f1':['City picture frame','$7.0'],'r':['4x5 entry rug','$35.0'],'f2':['flower vase','$14.0'],'n':['none and next','$0']}
-db = {'c':['shower curtains','$8.0'],'m':['wall mounted mirror','$20.0'],'s':['marble sink','$40.0'],'n':['none and next','$0']}
+dg={'p':['3 Pack garden flower','$5.0'],'l':['Hanging light wire','$10.0'],'b':['Garden bench','$35.0'],'n':['None and next','$0']}
+di={'t':['Small table lamp','$5.0'],'f1':['City picture frame','$7.0'],'r':['4x5 Entry rug','$35.0'],'f2':['Flower vase','$14.0'],'n':['None and next','$0']}
+db = {'c':['Shower curtains','$8.0'],'m':['Wall mounted mirror','$20.0'],'s':['Marble sink','$40.0'],'n':['None and next','$0']}
 
 class home:
     pass
@@ -25,7 +25,7 @@ def indoor_menu():
         for i in di:
            print(di[i][0],'(',i,')', ':',di[i][1])
 def bathroom_menu():
-        print('Indoor Options:')
+        print('Bathroom Options:')
         for i in db:
            print(db[i][0],'(',i,')', ':',db[i][1])
 def add_basket(basket,item):
@@ -33,44 +33,50 @@ def add_basket(basket,item):
 
 
 def main():
-    a,b,c=0,0,0
     basket=list()
     print('Welcome to Home Ideas Center, where all orders include a new home feeling!')
     print('For your new Home space ...')
-    x=input('Choose one type of garden idea (O for options, n for next category):')
-    a+=1
-    while x !='n' and a==1 :
-        garden_menu()
-        cd=input('Enter code of item')
-        cd=cd.split()
 
-        for i in range(len(cd)):
-            if cd[i] =='n':
-               break
+    while True:
+        x=input('Choose one type of garden idea (O for options, n for next category):')
+        if x=='n' or x=='N':
+            break
+        elif x=='o' or x=='O':
+            garden_menu()
+            cd=input('Enter code of item')
+            cd=cd.split()
 
-            else:
-              cod=cd[i]
-              basket.append(cod)
-        a+=1
+            for i in range(len(cd)):
+                if cd[i] =='n':
+                   break
+
+                else:
+                  cod=cd[i]
+                  basket.append(cod)
+        else:
+            print('Invalid option. Enter O for options, n for next category ')
 
             # nm=dg[cod][0]
             # it1=home_category('garden',cod,nm)
             # print_home_category(it1)
+    while True:
+        y=input('Choose one type of indoor idea (O for options, n for next category):')
+        if y=='n' or y=='N':
+            break
+        elif y=='o' or y=='O':
+            indoor_menu()
+            cd=input('Enter code of item')
+            cd=cd.split()
+            for i in range(len(cd)):
+             if cd[i] == 'n':
+                   break
+             else:
+                  cod=cd[i]
+                  basket.append(cod)
+        else:
+            print('Invalid option. Enter O for options, n for next category ')
 
-    y=input('Choose one type of indoor idea (O for options, n for next category):')
-    b+=1
 
-    while y !='n' and b==1 :
-        indoor_menu()
-        cd=input('Enter code of item')
-        cd=cd.split()
-        for i in range(len(cd)):
-         if cd[i] =='n':
-               break
-         else:
-              cod=cd[i]
-              basket.append(cod)
-        b+=1
 
 
 
